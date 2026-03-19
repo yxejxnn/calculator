05503188d90c0aa03effebdd1899825dfb6da4bf
정예진 <yxejxnn@gmail.com>
2026-03-19 18:42:12 +0900

    정수 및 연산자 입력 기능 추가

--- src/step1/App.java
+++ src/step1/App.java
@@ -1,7 +1,22 @@
 package step1;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+
+        // 스캐너 호출
+        Scanner scanner = new Scanner(System.in);
+
+        // 첫 번째 정수와 두 번째 정수를 입력받음
+        System.out.print("첫 번째 정수 입력 : ");
+        int num1 = scanner.nextInt();
+        System.out.print("두 번째 정수 입력 : ");
+        int num2 = scanner.nextInt();
+
+        // 연산자를 입력받음
+        System.out.print("연산자 입력 (+ - * /) : ");
+        char op = scanner.next().charAt(0); // 입력 받은 문자열의 첫 번째 문자를 저장
 
     }
 }
