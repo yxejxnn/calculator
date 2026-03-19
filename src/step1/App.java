91e7c6e8e1589ace230d04422dc42a6b43d8f01d
정예진 <yxejxnn@gmail.com>
2026-03-19 19:31:13 +0900

    사칙연산 기능 구현

--- src/step1/App.java
+++ src/step1/App.java
@@ -18,5 +18,40 @@ public class App {
         System.out.print("연산자 입력 (+ - * /) : ");
         char op = scanner.next().charAt(0); // 입력 받은 문자열의 첫 번째 문자를 저장
 
+        // 계산 결과를 저장할 변수
+        int result = 0;
+
+        // 입력 받은 값을 계산함
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("결과 : " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("결과 : " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("결과 : " + result);
+                break;
+
+            case '/':
+                // 나눗셈에서는 두 번째에 입력 받는 수는 0이 올 수 없음
+                if (num2 == 0) {
+                    System.out.println("0으로 나눌 수 없습니다.");
+                }
+                else {
+                    result = num1 / num2;
+                    System.out.println("결과 : " + result);
+                }
+                break;
+
+            default:
+                System.out.println("잘못된 연산자 입니다.");
+        }
+
     }
 }
