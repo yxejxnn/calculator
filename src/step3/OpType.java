8351a8bcc2eac4d85250387235b193942ec7b3da
정예진 <yxejxnn@gmail.com>
2026-03-24 17:46:25 +0900

    OpType 변환 메서드 추가

--- src/step3/OpType.java
+++ src/step3/OpType.java
@@ -4,5 +4,21 @@ public enum OpType {
     ADD,
     SUBTRACT,
     MULTIPLY,
-    DIVIDE
+    DIVIDE;
+
+    // 입력한 연산자 문자를 OpType으로 변환
+    public static OpType fromChar(char op) {
+        switch (op) {
+            case '+':
+                return ADD;
+            case '-':
+                return SUBTRACT;
+            case '*':
+                return MULTIPLY;
+            case '/':
+                return DIVIDE;
+            default:
+                return null;
+        }
+    }
 }
