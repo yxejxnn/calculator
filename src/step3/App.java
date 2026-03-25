8fdcdaffd0347a4813219cffc80ff79bc3547de6
정예진 <yxejxnn@gmail.com>
2026-03-25 13:50:04 +0900

    App 입력 Number 기반으로 변경

--- src/step3/App.java
+++ src/step3/App.java
@@ -7,24 +7,36 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        Calculator<Integer> calculator = new Calculator<>();
+        Calculator<Number> calculator = new Calculator<>();
 
         while (true) {
-            System.out.print("첫 번째 정수 입력 : ");
-            int num1 = scanner.nextInt();
-            System.out.print("두 번째 정수 입력 : ");
-            int num2 = scanner.nextInt();
-            System.out.println("연산자 입력 (+ - * /) : ");
+            System.out.print("첫 번째 숫자 입력 : ");
+            Number num1;
+            if (scanner.hasNextInt()) {
+                num1 = scanner.nextInt();
+            }
+            else {
+                num1 = scanner.nextDouble();
+            }
+            System.out.print("두 번째 숫자 입력 : ");
+            Number num2;
+            if (scanner.hasNextInt()) {
+                num2 = scanner.nextInt();
+            }
+            else {
+                num2 = scanner.nextDouble();
+            }
+            System.out.print("연산자 입력 (+ - * /) : ");
             char op = scanner.next().charAt(0);
 
             // 연산자를 OpType으로 변환
             OpType opType = OpType.fromChar(op);
 
             // Calculator 클래스를 이용해 계산 수행
-            Integer result = calculator.calculate(num1, num2, opType);
+            Number result = calculator.calculate(num1, num2, opType);
 
             // 0으로 나누는 경우가 아닐 때만 결과 출력
-            if (!(op == '/' && num2 == 0)) {
+            if (!(op == '/' && num2.doubleValue() == 0)) {
                 System.out.println("결과 : " + result);
             }
 
@@ -32,7 +44,7 @@ public class App {
             System.out.println("저장된 결과 : " + calculator.getResults());
 
             // 삭제 여부 입력
-            System.out.println("가장 먼저 저장된 결과를 삭제하시겠습니까? (yes 입력 시 삭제) : ");
+            System.out.print("가장 먼저 저장된 결과를 삭제하시겠습니까? (yes 입력 시 삭제) : ");
             String removeAnswer = scanner.next();
             if (removeAnswer.equals("yes")) {
                 calculator.removeResult();
@@ -40,7 +52,7 @@ public class App {
             }
 
             // 종료 여부 입력
-            System.out.println("계산을 계속 하겠습니까? (exit 입력 시 종료) : ");
+            System.out.print("계산을 계속 하겠습니까? (exit 입력 시 종료) : ");
             String answer = scanner.next();
 
             if (answer.equals("exit")) {
